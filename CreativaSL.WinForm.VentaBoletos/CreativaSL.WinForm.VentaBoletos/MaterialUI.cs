4ee7be50f067d748fe7b0e694b01819fd80ab5ac
EnocVZ <enoch.95@hotmail.com>
2024-03-28 19:23:15 -0600

    feat: new styles

---
+++ CreativaSL.WinForm.VentaBoletos/CreativaSL.WinForm.VentaBoletos/MaterialUI.cs
@@ -0,0 +1,26 @@
+﻿using MaterialSkin.Controls;
+using MaterialSkin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreativaSL.WinForm.VentaBoletos
+{
+    public class MaterialUI
+    {
+        public static MaterialSkinManager loadMaterial(MaterialForm actualForm)
+        {
+            // Create a material theme manager and add the form to manage (this)
+            MaterialSkinManager materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(actualForm);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+
+            // Configure color schema
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Indigo500, Primary.Indigo700, Primary.Indigo100, Accent.Pink200, TextShade.WHITE);
+            //materialSkinManager.ColorScheme = new ColorScheme(Primary.Blue800, Primary.Blue900,Primary.Blue500, Accent.Indigo700,TextShade.WHITE);
+            return materialSkinManager;
+        }
+    }
+}
