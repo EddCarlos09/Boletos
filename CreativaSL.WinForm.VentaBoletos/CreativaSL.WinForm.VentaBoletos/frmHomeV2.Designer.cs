7c909b6ffe0811ceae9948d225ce45a9d2d1fbb6
DavidRVF <143049524+DavidRVF@users.noreply.github.com>
2024-04-01 15:57:27 -0600

    cambiosfrmHomeV2

--- CreativaSL.WinForm.VentaBoletos/CreativaSL.WinForm.VentaBoletos/frmHomeV2.Designer.cs
+++ CreativaSL.WinForm.VentaBoletos/CreativaSL.WinForm.VentaBoletos/frmHomeV2.Designer.cs
@@ -28,8 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHomeV2));
             this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.materialTabControl3 = new MaterialSkin.Controls.MaterialTabControl();
+            this.tabPage14 = new System.Windows.Forms.TabPage();
+            this.tabPage15 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
@@ -40,7 +45,10 @@
             this.tabPage9 = new System.Windows.Forms.TabPage();
             this.tabPage10 = new System.Windows.Forms.TabPage();
             this.tabPage11 = new System.Windows.Forms.TabPage();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.materialTabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.materialTabControl3.SuspendLayout();
             this.SuspendLayout();
             // 
             // materialTabControl1
@@ -57,102 +65,147 @@
             this.materialTabControl1.Controls.Add(this.tabPage10);
             this.materialTabControl1.Controls.Add(this.tabPage11);
             this.materialTabControl1.Depth = 0;
+            this.materialTabControl1.ImageList = this.imageList1;
             this.materialTabControl1.Location = new System.Drawing.Point(6, 67);
             this.materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialTabControl1.Multiline = true;
             this.materialTabControl1.Name = "materialTabControl1";
             this.materialTabControl1.SelectedIndex = 0;
-            this.materialTabControl1.Size = new System.Drawing.Size(788, 377);
+            this.materialTabControl1.Size = new System.Drawing.Size(988, 254);
             this.materialTabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
+            this.tabPage1.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage1.Controls.Add(this.materialTabControl3);
+            this.tabPage1.ImageKey = "icons8-checkout-96.png";
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(780, 348);
+            this.tabPage1.Size = new System.Drawing.Size(980, 225);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Ventas";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // materialTabControl3
+            // 
+            this.materialTabControl3.Controls.Add(this.tabPage14);
+            this.materialTabControl3.Controls.Add(this.tabPage15);
+            this.materialTabControl3.Depth = 0;
+            this.materialTabControl3.Location = new System.Drawing.Point(624, 152);
+            this.materialTabControl3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabControl3.Multiline = true;
+            this.materialTabControl3.Name = "materialTabControl3";
+            this.materialTabControl3.SelectedIndex = 0;
+            this.materialTabControl3.Size = new System.Drawing.Size(8, 8);
+            this.materialTabControl3.TabIndex = 0;
+            // 
+            // tabPage14
+            // 
+            this.tabPage14.Location = new System.Drawing.Point(4, 46);
+            this.tabPage14.Name = "tabPage14";
+            this.tabPage14.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage14.Size = new System.Drawing.Size(0, 0);
+            this.tabPage14.TabIndex = 0;
+            this.tabPage14.Text = "tabPage14";
+            this.tabPage14.UseVisualStyleBackColor = true;
+            // 
+            // tabPage15
+            // 
+            this.tabPage15.Location = new System.Drawing.Point(4, 46);
+            this.tabPage15.Name = "tabPage15";
+            this.tabPage15.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage15.Size = new System.Drawing.Size(0, 0);
+            this.tabPage15.TabIndex = 1;
+            this.tabPage15.Text = "tabPage15";
+            this.tabPage15.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
+            this.tabPage2.ImageKey = "icons8-checkout-96.png";
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(780, 348);
+            this.tabPage2.Size = new System.Drawing.Size(922, 106);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Ventas G";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage3
             // 
+            this.tabPage3.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage3.ImageKey = "icons8-book-96.png";
             this.tabPage3.Location = new System.Drawing.Point(4, 25);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(780, 348);
+            this.tabPage3.Size = new System.Drawing.Size(922, 106);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Apartados";
-            this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // tabPage4
             // 
+            this.tabPage4.ImageKey = "icons8-account-96.png";
             this.tabPage4.Location = new System.Drawing.Point(4, 25);
             this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(780, 348);
+            this.tabPage4.Size = new System.Drawing.Size(922, 106);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "Clientes";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
             // tabPage5
             // 
+            this.tabPage5.ImageKey = "icons8-box-96.png";
             this.tabPage5.Location = new System.Drawing.Point(4, 25);
             this.tabPage5.Name = "tabPage5";
-            this.tabPage5.Size = new System.Drawing.Size(780, 348);
+            this.tabPage5.Size = new System.Drawing.Size(922, 106);
             this.tabPage5.TabIndex = 4;
             this.tabPage5.Text = "Caja";
             this.tabPage5.UseVisualStyleBackColor = true;
             // 
             // tabPage6
             // 
+            this.tabPage6.ImageKey = "icons8-suitcase-96.png";
             this.tabPage6.Location = new System.Drawing.Point(4, 25);
             this.tabPage6.Name = "tabPage6";
-            this.tabPage6.Size = new System.Drawing.Size(780, 348);
+            this.tabPage6.Size = new System.Drawing.Size(922, 106);
             this.tabPage6.TabIndex = 5;
             this.tabPage6.Text = "Maletas";
             this.tabPage6.UseVisualStyleBackColor = true;
             // 
             // tabPage7
             // 
+            this.tabPage7.ImageKey = "icons8-admin-96.png";
             this.tabPage7.Location = new System.Drawing.Point(4, 25);
             this.tabPage7.Name = "tabPage7";
-            this.tabPage7.Size = new System.Drawing.Size(780, 348);
+            this.tabPage7.Size = new System.Drawing.Size(922, 106);
             this.tabPage7.TabIndex = 6;
             this.tabPage7.Text = "Admin";
             this.tabPage7.UseVisualStyleBackColor = true;
             // 
             // tabPage8
             // 
+            this.tabPage8.ImageKey = "icons8-document-50.png";
             this.tabPage8.Location = new System.Drawing.Point(4, 25);
             this.tabPage8.Name = "tabPage8";
-            this.tabPage8.Size = new System.Drawing.Size(780, 348);
+            this.tabPage8.Size = new System.Drawing.Size(922, 106);
             this.tabPage8.TabIndex = 7;
             this.tabPage8.Text = "Reportes";
             this.tabPage8.UseVisualStyleBackColor = true;
             // 
             // tabPage9
             // 
+            this.tabPage9.ImageKey = "icons8-settings-50.png";
             this.tabPage9.Location = new System.Drawing.Point(4, 25);
             this.tabPage9.Name = "tabPage9";
-            this.tabPage9.Size = new System.Drawing.Size(780, 348);
+            this.tabPage9.Size = new System.Drawing.Size(922, 106);
             this.tabPage9.TabIndex = 8;
             this.tabPage9.Text = "Config";
             this.tabPage9.UseVisualStyleBackColor = true;
             // 
             // tabPage10
             // 
+            this.tabPage10.ImageKey = "icons8-administrator-male-96.png";
             this.tabPage10.Location = new System.Drawing.Point(4, 25);
             this.tabPage10.Name = "tabPage10";
-            this.tabPage10.Size = new System.Drawing.Size(780, 348);
+            this.tabPage10.Size = new System.Drawing.Size(922, 106);
             this.tabPage10.TabIndex = 9;
             this.tabPage10.Text = "Asistencia";
             this.tabPage10.UseVisualStyleBackColor = true;
@@ -161,21 +214,38 @@
             // 
             this.tabPage11.Location = new System.Drawing.Point(4, 25);
             this.tabPage11.Name = "tabPage11";
-            this.tabPage11.Size = new System.Drawing.Size(780, 348);
+            this.tabPage11.Size = new System.Drawing.Size(922, 106);
             this.tabPage11.TabIndex = 10;
             this.tabPage11.Text = "Salir";
             this.tabPage11.UseVisualStyleBackColor = true;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "icons8-checkout-96.png");
+            this.imageList1.Images.SetKeyName(1, "icons8-checkout-96.png");
+            this.imageList1.Images.SetKeyName(2, "icons8-book-96.png");
+            this.imageList1.Images.SetKeyName(3, "icons8-account-96.png");
+            this.imageList1.Images.SetKeyName(4, "icons8-box-96.png");
+            this.imageList1.Images.SetKeyName(5, "icons8-suitcase-96.png");
+            this.imageList1.Images.SetKeyName(6, "icons8-admin-96.png");
+            this.imageList1.Images.SetKeyName(7, "icons8-document-50.png");
+            this.imageList1.Images.SetKeyName(8, "icons8-settings-50.png");
+            this.imageList1.Images.SetKeyName(9, "icons8-administrator-male-96.png");
             // 
             // frmHomeV2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1264, 565);
             this.Controls.Add(this.materialTabControl1);
             this.Name = "frmHomeV2";
             this.Text = "frmHomeV2";
             this.Load += new System.EventHandler(this.frmHomeV2_Load);
             this.materialTabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.materialTabControl3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -194,5 +264,9 @@
         private System.Windows.Forms.TabPage tabPage9;
         private System.Windows.Forms.TabPage tabPage10;
         private System.Windows.Forms.TabPage tabPage11;
+        private System.Windows.Forms.ImageList imageList1;
+        private MaterialSkin.Controls.MaterialTabControl materialTabControl3;
+        private System.Windows.Forms.TabPage tabPage14;
+        private System.Windows.Forms.TabPage tabPage15;
     }
 }
