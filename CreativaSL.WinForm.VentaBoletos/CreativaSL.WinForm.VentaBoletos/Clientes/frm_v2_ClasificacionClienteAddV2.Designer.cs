5758383fe5140084d541433479a84fa279325dfa
DavidRVF <143049524+DavidRVF@users.noreply.github.com>
2024-04-11 10:15:15 -0600

    Reapply "Merge branch 'Desarrollo' into Desarrollo-Dav"
    This reverts commit 1d63c12695abffcb82767a0c18c7804c66c83020.

---
+++ CreativaSL.WinForm.VentaBoletos/CreativaSL.WinForm.VentaBoletos/Clientes/frm_v2_ClasificacionClienteAddV2.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace CreativaSL.WinForm.VentaBoletos.Clientes
+{
+    partial class frm_v2_ClasificacionClienteAddV2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_v2_ClasificacionClienteAddV2));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txt_mensaje = new System.Windows.Forms.TextBox();
+            this.txtPorcentaje = new MaterialSkin.Controls.MaterialTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtClasificacion = new MaterialSkin.Controls.MaterialTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btn_Cancelar = new System.Windows.Forms.Button();
+            this.btn_Guardar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.txt_mensaje);
+            this.panel1.Controls.Add(this.btn_Cancelar);
+            this.panel1.Controls.Add(this.btn_Guardar);
+            this.panel1.Controls.Add(this.txtPorcentaje);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtClasificacion);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Location = new System.Drawing.Point(3, 64);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(811, 478);
+            this.panel1.TabIndex = 0;
+            // 
+            // txt_mensaje
+            // 
+            this.txt_mensaje.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt_mensaje.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.txt_mensaje.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_mensaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_mensaje.Location = new System.Drawing.Point(162, 290);
+            this.txt_mensaje.Multiline = true;
+            this.txt_mensaje.Name = "txt_mensaje";
+            this.txt_mensaje.ReadOnly = true;
+            this.txt_mensaje.Size = new System.Drawing.Size(390, 38);
+            this.txt_mensaje.TabIndex = 17;
+            this.txt_mensaje.Text = "Ocurrió un Error";
+            this.txt_mensaje.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_mensaje.Visible = false;
+            // 
+            // txtPorcentaje
+            // 
+            this.txtPorcentaje.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtPorcentaje.AnimateReadOnly = false;
+            this.txtPorcentaje.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPorcentaje.Depth = 0;
+            this.txtPorcentaje.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.txtPorcentaje.LeadingIcon = null;
+            this.txtPorcentaje.Location = new System.Drawing.Point(162, 221);
+            this.txtPorcentaje.MaxLength = 50;
+            this.txtPorcentaje.MouseState = MaterialSkin.MouseState.OUT;
+            this.txtPorcentaje.Multiline = false;
+            this.txtPorcentaje.Name = "txtPorcentaje";
+            this.txtPorcentaje.Size = new System.Drawing.Size(390, 50);
+            this.txtPorcentaje.TabIndex = 7;
+            this.txtPorcentaje.Text = "3";
+            this.txtPorcentaje.TrailingIcon = null;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.Gray;
+            this.label2.Location = new System.Drawing.Point(278, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Porcentaje";
+            // 
+            // txtClasificacion
+            // 
+            this.txtClasificacion.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtClasificacion.AnimateReadOnly = false;
+            this.txtClasificacion.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtClasificacion.Depth = 0;
+            this.txtClasificacion.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.txtClasificacion.LeadingIcon = null;
+            this.txtClasificacion.Location = new System.Drawing.Point(162, 120);
+            this.txtClasificacion.MaxLength = 50;
+            this.txtClasificacion.MouseState = MaterialSkin.MouseState.OUT;
+            this.txtClasificacion.Multiline = false;
+            this.txtClasificacion.Name = "txtClasificacion";
+            this.txtClasificacion.Size = new System.Drawing.Size(390, 50);
+            this.txtClasificacion.TabIndex = 5;
+            this.txtClasificacion.Text = "Cliente VIP";
+            this.txtClasificacion.TrailingIcon = null;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(278, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Clasificación";
+            // 
+            // label8
+            // 
+            this.label8.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Navy;
+            this.label8.Location = new System.Drawing.Point(227, 37);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(256, 26);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Clasificación de clientes";
+            // 
+            // btn_Cancelar
+            // 
+            this.btn_Cancelar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_Cancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Cancelar.ForeColor = System.Drawing.Color.DimGray;
+            this.btn_Cancelar.Image = ((System.Drawing.Image)(resources.GetObject("btn_Cancelar.Image")));
+            this.btn_Cancelar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_Cancelar.Location = new System.Drawing.Point(380, 346);
+            this.btn_Cancelar.Name = "btn_Cancelar";
+            this.btn_Cancelar.Padding = new System.Windows.Forms.Padding(10, 0, 10, 5);
+            this.btn_Cancelar.Size = new System.Drawing.Size(132, 109);
+            this.btn_Cancelar.TabIndex = 63;
+            this.btn_Cancelar.Text = "Cancelar";
+            this.btn_Cancelar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_Cancelar.UseVisualStyleBackColor = true;
+            this.btn_Cancelar.Click += new System.EventHandler(this.btn_Cancelar_Click_1);
+            // 
+            // btn_Guardar
+            // 
+            this.btn_Guardar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_Guardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Guardar.ForeColor = System.Drawing.Color.DimGray;
+            this.btn_Guardar.Image = ((System.Drawing.Image)(resources.GetObject("btn_Guardar.Image")));
+            this.btn_Guardar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_Guardar.Location = new System.Drawing.Point(197, 346);
+            this.btn_Guardar.Name = "btn_Guardar";
+            this.btn_Guardar.Padding = new System.Windows.Forms.Padding(10, 0, 10, 5);
+            this.btn_Guardar.Size = new System.Drawing.Size(129, 109);
+            this.btn_Guardar.TabIndex = 62;
+            this.btn_Guardar.Text = "Guardar";
+            this.btn_Guardar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_Guardar.UseVisualStyleBackColor = true;
+            this.btn_Guardar.Click += new System.EventHandler(this.btn_Guardar_Click);
+            // 
+            // frm_v2_ClasificacionClienteAddV2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 545);
+            this.Controls.Add(this.panel1);
+            this.Name = "frm_v2_ClasificacionClienteAddV2";
+            this.Text = "frm_v2_ClasificacionClienteAddV2";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label1;
+        private MaterialSkin.Controls.MaterialTextBox txtClasificacion;
+        private System.Windows.Forms.Label label2;
+        private MaterialSkin.Controls.MaterialTextBox txtPorcentaje;
+        private System.Windows.Forms.TextBox txt_mensaje;
+        private System.Windows.Forms.Button btn_Cancelar;
+        private System.Windows.Forms.Button btn_Guardar;
+    }
+}
